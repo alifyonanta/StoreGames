f6164abd75aca2321494d1b4e0d9d2c293d0984a
Alif Yonanta <28886550+alifyonanta@users.noreply.github.com>
2022-12-21 18:49:46 +0700

    Versi 1.4
    - Setup sign up form
    - Get value selection option
    - Upload foto dan integrasi sign up

--- pages/sign-up-photo.tsx
+++ pages/sign-up-photo.tsx
@@ -1,6 +1,59 @@
 import Image from "next/image";
+import { useCallback, useEffect, useState } from "react";
+import { setSignUp } from "../services/auth";
+import { getGameCategory } from "../services/player";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function SignUpPhoto() {
+  const [categories, setCategories] = useState([]);
+  const [favorite, setFavorite] = useState('');
+  const [image, setImage] = useState('');
+  const [imagePreview, setImagePreview] = useState('/icon/upload.svg');
+  const [localForm, setLocalForm] = useState({
+    name: '',
+    email: '',
+  });
+
+  const getGameCategoryAPI = useCallback(async() => {
+    const data = await getGameCategory();
+    console.log('data : ', data)
+    setCategories(data);
+    setFavorite(data[0]._id)
+  }, []);
+
+  useEffect(() => {
+    getGameCategoryAPI();
+  }, []);
+
+  useEffect(() => {
+    const getLocalForm = localStorage.getItem('user-form');
+    setLocalForm(JSON.parse(getLocalForm));
+  }, []);
+
+  const onSubmit = () => {
+    console.log('favorite ', favorite );
+    console.log('image ', image);
+    const getlocalForm = localStorage.getItem('user-form');
+    const form = JSON.parse(getlocalForm);
+    const data = new FormData();
+
+    data.append('email', form.email);
+    data.append('password', form.password);
+    data.append('phoneNumber', '081123123');
+    data.append('username', form.name);
+    data.append('name', form.name);
+    data.append('image', image)
+    data.append('favorite', favorite);
+    //data.append('role', 'user');
+    //data.append('status', 'Y');
+    const result = setSignUp(data);
+    if(result.error === 1) {
+      toast.error(result.message)
+    }
+    console.log('result : ', result)
+  }
+
   return (
     <section className="sign-up-photo mx-auto pt-lg-227 pb-lg-227 pt-130 pb-50">
       <div className="container mx-auto">
@@ -9,26 +62,31 @@ export default function SignUpPhoto() {
             <div>
               <div className="mb-20">
                 <div className="image-upload text-center">
-                  <label for="avatar">
-                    <Image src="/icon/upload.svg" width={120} height={120} alt="upload" />
+                  <label htmlFor="avatar">
+                    <img src={imagePreview} className='img-upload' />
+                    {/* <Image src={imagePreview} width={120} height={120} objectFit='cover' alt="upload" /> */}
                   </label>
-                  <input
+                  <input 
                     id="avatar"
                     type="file"
                     name="avatar"
                     accept="image/png, image/jpeg"
+                    onChange={(event) => {
+                      setImagePreview(URL.createObjectURL(event.target.files[0]))
+                      setImage(event.target.files[0])}
+                    } 
                   />
                 </div>
               </div>
               <h2 className="fw-bold text-xl text-center color-palette-1 m-0">
-                Shayna Anne
+                {localForm.name}
               </h2>
               <p className="text-lg text-center color-palette-1 m-0">
-                shayna@anne.com
+                {localForm.email}
               </p>
               <div className="pt-50 pb-50">
                 <label
-                  for="category"
+                  htmlFor="category"
                   className="form-label text-lg fw-medium color-palette-1 mb-10"
                 >
                   Favorite Game
@@ -38,26 +96,27 @@ export default function SignUpPhoto() {
                   name="category"
                   className="form-select d-block w-100 rounded-pill text-lg"
                   aria-label="Favorite Game"
+                  value={favorite}
+                  onChange={(event) => {
+                    setFavorite(event.target.value)
+                  }}
                 >
-                  <option value="" disabled selected>
-                    Select Category
-                  </option>
-                  <option value="fps">First Person Shoter</option>
-                  <option value="rpg">Role Playing Game</option>
-                  <option value="arcade">Arcade</option>
-                  <option value="sport">Sport</option>
+                  {categories.map(category => {
+                    return <option key={category._id} value={category._id} selected>{category.name}</option>
+                  })}
                 </select>
               </div>
             </div>
 
             <div className="button-group d-flex flex-column mx-auto">
-              <a
+              <button
+                type="button"
                 className="btn btn-create fw-medium text-lg text-white rounded-pill mb-16"
-                href="./sign-up-photo-success.html"
                 role="button"
+                onClick={onSubmit}
               >
                 Create My Account
-              </a>
+              </button>
               <a
                 className="btn btn-tnc text-lg color-palette-1 text-decoration-underline pt-15"
                 href="#"
@@ -69,6 +128,7 @@ export default function SignUpPhoto() {
           </div>
         </form>
       </div>
+      <ToastContainer />
     </section>
   );
 }
